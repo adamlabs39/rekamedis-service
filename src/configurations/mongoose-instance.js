99defa84462cceb62c1101d447978c0d8861b0a4
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-09-26 14:59:28 +0700

    setup docker

--- src/configurations/mongoose-instance.js
+++ src/configurations/mongoose-instance.js
@@ -4,10 +4,12 @@ import "dotenv/config";
 const DB_HOST = process.env.MONGO_DB_HOST || 'localhost';
 const DB_PORT = process.env.MONGO_DB_PORT || 27017;
 const DB_NAME = process.env.MONGO_DB_NAME || 'adameds';
+const DB_USER = process.env.MONGO_DB_USERNAME || 'admin';
+const DB_PASSWORD = process.env.MONGO_DB_PASSWORD || 'admin';
 
 const mongooseInstance = async () => {
     try {
-        await mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}?replicaSet=rs0`);
+        await mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB", error);
